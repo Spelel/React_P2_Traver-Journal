458f08fdbb095a7f5223c18843198ce54026ed42
Arek <arek-karczo.wp.pl@wp.pl>
2025-06-09 21:54:18 +0200

    Header and first item layout with initial styling

---
+++ src/components/header.jsx
@@ -0,0 +1,10 @@
+import globe from '../assets/globe.png'
+export default function Header() {
+    return (
+        <>
+        <h1>
+            <img src={globe}/> my travel journal.
+        </h1>
+        </>
+    )
+}
